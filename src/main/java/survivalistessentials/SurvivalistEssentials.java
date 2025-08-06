235ec6b55fb661b077772fea8ae4edb1a09aa269
Wendall Cada <wendallc@83864.com>
2025-08-06 12:33:20 -0700

    Fix improper proxy handling. Resolves #18

--- src/main/java/survivalistessentials/SurvivalistEssentials.java
+++ src/main/java/survivalistessentials/SurvivalistEssentials.java
@@ -3,12 +3,10 @@ package survivalistessentials;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import survivalistessentials.proxy.ClientProxy;
 import survivalistessentials.proxy.CommonProxy;
 
 @Mod(SurvivalistEssentials.MODID)
@@ -23,12 +21,8 @@ public class SurvivalistEssentials {
     public SurvivalistEssentials() {
         BUS = FMLJavaModLoadingContext.get().getModEventBus();
 
-        if (FMLEnvironment.dist.isClient()) {
-            BUS.register(new ClientProxy());
-        }
-        else {
-            BUS.register(new CommonProxy());
-        }
+        PROXY = new CommonProxy();
+        BUS.register(PROXY);
 
         PROXY.start();
     }
