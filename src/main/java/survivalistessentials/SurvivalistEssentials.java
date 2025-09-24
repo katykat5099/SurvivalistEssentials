51a0f2df4805a865133ded7bd91ec555f538453b
Wendall Cada <wendallc@83864.com>
2025-09-24 11:47:40 -0700

    Update logger to use slf4j as it is more secure.

--- src/main/java/survivalistessentials/SurvivalistEssentials.java
+++ src/main/java/survivalistessentials/SurvivalistEssentials.java
@@ -4,8 +4,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import survivalistessentials.proxy.CommonProxy;
 
@@ -13,11 +13,12 @@ import survivalistessentials.proxy.CommonProxy;
 public class SurvivalistEssentials {
 
     public static final String MODID = "survivalistessentials";
-    public static final Logger LOGGER = LogManager.getFormatterLogger(SurvivalistEssentials.MODID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(SurvivalistEssentials.MODID);
 
     public static CommonProxy PROXY;
     public static IEventBus BUS;
 
+    @SuppressWarnings("removal")
     public SurvivalistEssentials() {
         BUS = FMLJavaModLoadingContext.get().getModEventBus();
 
